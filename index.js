f2bb0683ee534a7ecb53708dd37c75f9b3530ea1
Divij Sharma <divijs75@gmail.com>
2024-10-20 03:25:32 +0530

    feat(server): Construct response packet

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const server = dgram.createSocket('udp4');
 
 server.on('message', (msg, rinfo) => {
         const request = dnsPacket.decode(msg);
+        const IP = DNSDB[request.questions[0].name];
+
+        const packet = dnsPacket.encode({
+            type: 'response',
+            id: request.id,
+            flags: dnsPacket.AUTHORITATIVE_ANSWER,
+            questions: request.questions,
+            answers: [{
+                type: IP.type,
+                class: 'IN',
+                name: request.questions[0].name,
+                ttl: 300,
+                data: IP.data
+            }]
+        })
+    
+        server.send(packet, rinfo.port, rinfo.address);
+
         console.log(request.questions)
         console.log(rinfo)
     }
